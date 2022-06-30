1437fcf0432ed1045eb261304fd685f19500a1f3
Aniket Godambe <65593787+AniketGodambe@users.noreply.github.com>
2022-06-30 20:23:45 +0530

    Final changes

--- internet_detect/lib/home_page.dart
+++ internet_detect/lib/home_page.dart
@@ -12,37 +12,37 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String status = "Checking status";
-  Connectivity _connectivity = Connectivity();
+  final Connectivity _connectivity = Connectivity();
   late StreamSubscription _streamSubscription;
   @override
   void initState() {
-    // checkConnectivity();
     checkinit();
     super.initState();
   }
+//ontap to check connectivity
+  // void checkConnectivity() async {
+  //   var connectionResult = await _connectivity.checkConnectivity();
+  //   if (connectionResult == ConnectivityResult.mobile) {
+  //     setState(() {
+  //       status = status;
+  //     });
+  //     print("Connected");
+  //   } else if (connectionResult == ConnectivityResult.wifi) {
+  //     status = "Connected";
+  //     setState(() {
+  //       status = status;
+  //     });
+  //     print("Connected");
+  //   } else {
+  //     status = "Not Connected";
+  //     print("Not Connected");
+  //     setState(() {
+  //       status = status;
+  //     });
+  //   }
+  // }
 
-  void checkConnectivity() async {
-    var connectionResult = await _connectivity.checkConnectivity();
-    if (connectionResult == ConnectivityResult.mobile) {
-      setState(() {
-        status = status;
-      });
-      print("Connected");
-    } else if (connectionResult == ConnectivityResult.wifi) {
-      status = "Connected";
-      setState(() {
-        status = status;
-      });
-      print("Connected");
-    } else {
-      status = "Not Connected";
-      print("Not Connected");
-      setState(() {
-        status = status;
-      });
-    }
-  }
-
+//auto detect connectivity
   void checkinit() {
     _streamSubscription = _connectivity.onConnectivityChanged.listen((event) {
       if (event == ConnectivityResult.mobile) {
@@ -77,12 +77,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        child: status == "Connected"
-            ? const Text("Your are online now")
-            : const Text("Your are offline now"),
-      )),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Intenet Connectivity"),
+        ),
+        body: status == "Connected"
+            ? Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: const Text(
+                    "Welcome Scren",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              )
+            : Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: const Text(
+                    "Check your Intenet connection",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ));
   }
 }
